583a2682c60fdf0e3c3617b24bb9138f5aa84785
Robert Santos <robertsantosti@outlook.com>
2022-06-21 12:16:40 -0300

    Implementação do header da aplicação

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,3 +1,7 @@
+import { SharedModule } from './../../shared/shared.module';
+import { SidebarMenuComponent } from './../../layouts/sidebar-menu/sidebar-menu.component';
+import { FooterComponent } from './../../layouts/footer/footer.component';
+import { HeaderComponent } from './../../layouts/header/header.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -6,7 +10,12 @@ import { HOME_ROUTES } from './../../core/routes/home.routes';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule.forChild(HOME_ROUTES)],
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarMenuComponent,
+  ],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(HOME_ROUTES)],
 })
 export class HomeModule {}
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule, MatBadgeModule],
+  exports: [MatButtonModule, MatIconModule, MatBadgeModule],
 })
 export class MaterialModule {}
